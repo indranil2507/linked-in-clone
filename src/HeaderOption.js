8e15280d40a1280d13c014994b9601237dc506fd
indranil2507 <iindranilsaha1@gmail.com>
2021-06-21 22:54:35 +0530

    first commit

---
+++ src/HeaderOption.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./HeaderOption.css"
+import {Avatar} from "@material-ui/core";
+
+function HeaderOption({avatar,Icon,title}) {
+    return (
+        <div className="headerOption">
+            {Icon && <Icon className="headerOption__icon"/>}
+            {avatar && 
+                <Avatar className="headerOption__icon" src={avatar}/>
+            
+            }
+            <h5 className="headerOption_title">{title}</h5>
+        </div>
+    )
+}
+
+export default HeaderOption
+ 
